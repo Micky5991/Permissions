21c721316a3876752d11a2f4e5a68b73ff5ae05e
Francesco Paolocci <micky5991@gmail.com>
2024-12-29 20:54:19 +0100

    Add context and comments to the request

--- Permissions/Decision/AccessRequest.cs
+++ Permissions/Decision/AccessRequest.cs
@@ -4,11 +4,27 @@ namespace Permissions.Decision;
 
 public sealed class AccessRequest
 {
+    /// <summary>
+    /// Subject that tries to access a certain <see cref="Resource"/> with the specified <see cref="Action"/>.
+    /// </summary>
     public SubjectIdentifier Subject { get; }
+
+    /// <summary>
+    /// Resource that will be accessed by the given <see cref="Subject"/>.
+    /// </summary>
     public ResourceIdentifier Resource { get; }
+
+    /// <summary>
+    /// Action that will be applied to the specified <see cref="Resource"/> by the <see cref="Subject"/>.
+    /// </summary>
     public ResourceAction Action { get; }
 
-    public AccessRequest(SubjectIdentifier subject, ResourceIdentifier resource, ResourceAction action)
+    /// <summary>
+    /// Extra environment data of this access request.
+    /// </summary>
+    public Dictionary<string, object> Context { get; set; }
+
+    public AccessRequest(SubjectIdentifier subject, ResourceIdentifier resource, ResourceAction action, Dictionary<string, object>? context = null)
     {
         Guard.IsNotNull(subject);
         Guard.IsNotNull(resource);
@@ -17,5 +33,7 @@ public sealed class AccessRequest
         Subject = subject;
         Resource = resource;
         Action = action;
+
+        Context = context ?? new Dictionary<string, object>();
     }
 }
