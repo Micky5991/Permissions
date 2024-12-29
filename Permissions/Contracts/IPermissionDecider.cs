047129f0176b0e4936327d94656d7cae6348026c
Francesco Paolocci <micky5991@gmail.com>
2024-12-29 20:56:43 +0100

    Add decision results and authorize method to the decider

---
+++ Permissions/Contracts/IPermissionDecider.cs
@@ -0,0 +1,12 @@
+﻿using Permissions.Decision;
+
+namespace Permissions.Contracts;
+
+/// <summary>
+/// Main logic evaluator and decider for incoming requests. This instance finally decides if the requested access is granted or not.
+/// </summary>
+/// <remarks>This type represents the PDP (Policy Decision Point) in the ABAC architecture.</remarks>
+public interface IPermissionDecider
+{
+    public Task<AuthorizationDecision> AuthorizeAsync(AccessRequest request);
+}
---
+++ Permissions/Decision/AuthorizationDecision.cs
@@ -0,0 +1,19 @@
+﻿namespace Permissions.Decision;
+
+public enum AuthorizationDecision
+{
+    /// <summary>
+    /// Denies access to this resource action fully.
+    /// </summary>
+    Deny,
+
+    /// <summary>
+    /// Denies access to this resource action fully, but this can be used to still allow the user to see the existence of this action.
+    /// </summary>
+    Disable,
+
+    /// <summary>
+    /// Grants full access to this resource action.
+    /// </summary>
+    Allow,
+}
